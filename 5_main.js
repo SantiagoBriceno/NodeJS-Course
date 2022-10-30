96e51393961ed99c2c28458f1a20033179075231
Santiago <Santiagogbf2000@Gmail.com>
2022-10-30 11:43:08 -0400

    estructura de modulos para NODEJS

---
+++ 5_main.js
@@ -0,0 +1,6 @@
+//Control + espacio y me dice que atributos tiene el objeto que exporto
+
+const {myArray, myWebAddress, myNumber} = require('./module/1module');
+console.log(myArray);
+console.log(myWebAddress);
+console.log(myNumber);
---
+++ module/1module.js
@@ -0,0 +1,17 @@
+//Exportar modulos o partes de codigo a nuestra app principal
+const myWebAddress = "faztweb.com";
+const myNumber = 30
+const myArray = [10, 20, 30]
+const user = {
+    name: "John",
+    last: "ray"
+}
+
+module.exports = {
+    myWebAddress,
+    myNumber,
+    myArray,
+    user
+}
+
+
