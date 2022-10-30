b98207d0ceae668cbd38f6ef546275e20889ef61
Santiago <Santiagogbf2000@Gmail.com>
2022-10-30 10:55:46 -0400

    logica JS

---
+++ 2_LogicaJS.js
@@ -0,0 +1,35 @@
+// let username = 'Santi'
+// let age = 30
+// let hasHobbies = true
+// let points = [10, 20, 30]
+// let user = {
+//     name: 'ryan',
+//     lastname: 'ray'
+// }
+// const PI = 3.1415
+
+// console.log(username)
+// console.log(age)
+// console.log(hasHobbies)
+// console.log(points)
+// console.log(user)
+// console.log(PI)
+
+// let newArray = points.map(number => number * 2)
+// console.log(newArray)
+
+//CONDICIONALES
+// const age = 30
+// if (age >= 18){
+//     console.log("adult")
+// }else{
+//     console.log("child")
+// }
+//RECORRER ARREGLO
+// const names = ['joe', 'John', 'marco']
+// names.map(name => console.log(name));
+
+let showUserInfo = (username, userAge) =>
+  `The username is ${username}, the user age is ${userAge}`;
+
+console.log(showUserInfo("Santiago", 21));
