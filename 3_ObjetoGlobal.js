2ac37c8796213c4a89b59570f4d579b568eaa63f
Santiago <Santiagogbf2000@Gmail.com>
2022-10-30 11:06:52 -0400

    Objetos globales mas usados

---
+++ 3_ObjetoGlobal.js
@@ -0,0 +1,17 @@
+//Objeto dirName
+//Retorna la ruta de la ubicacion donde se guarda el archivo
+console.log(__dirname)
+
+//file name
+//Retorna la direccion en memoria o ruta completa del archivo
+console.log(__filename)
+
+//modulo
+//Objeto con informacion del archivo que se ejecuta
+//Permite separar una aplicacion de JS en multiples archivos
+console.log(module)
+
+//Funcion require
+//Metodo que permite importar los archivos que componen una aplicacion
+console.log(require)
+
