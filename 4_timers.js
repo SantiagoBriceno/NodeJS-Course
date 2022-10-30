6fa52674fda7dcbf3c69b63b6018397a43513810
Santiago <Santiagogbf2000@Gmail.com>
2022-10-30 11:29:17 -0400

    timers importantes para NodeJs

---
+++ 4_timers.js
@@ -0,0 +1,10 @@
+//Ejecuta un metodo cada cierto tiempo, ingresado por parametro en milisegundos
+
+// setInterval(()=>{
+//     console.log("Hello world!");
+// }, 2000);
+
+//Metodo que retorna una funcion al cabo de cierto tiempo, ingresado por parametro en milisegundos
+setTimeout(()=>{
+    console.log("Hello world!");
+}, 5000); 
