672d686ef05126eb37af050a4c048d5c2a20c61b
Santiago <Santiagogbf2000@Gmail.com>
2022-10-30 13:08:12 -0400

    Primer servido con el modulo HTTP de Node

---
+++ 11_moduloHTTP.js
@@ -0,0 +1,10 @@
+//Crear un servidor que pueda permitir recibir peticiones
+//interpretarlas y mandar respuestas
+
+const http = require('http');
+http.createServer((request, response) => {
+    response.write('hello World');
+    response.end();
+}).listen(3000);
+
+console.log('servidor escuchando en el puerto 3000');
